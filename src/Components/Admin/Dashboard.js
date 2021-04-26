49e4f2ed8e4182cb5daacd663c3ca83426ee6bc2
maxly20 <maxly2011@gmail.com>
2021-04-26 16:52:58 +1000

    building dashboard

--- src/Components/Admin/Dashboard.js
+++ src/Components/Admin/Dashboard.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import AdminLayout from '../../Hoc/AdminLayout'
+import AdminLayout from '../../Hoc/AdminLayout';
 
-const Dashboard = (props) => {
-
-    return(
-        <AdminLayout>
-           <div className="user_dashboard">
-                <div>
-                    This is your dashboard
-                </div>
-           </div>
-        </AdminLayout> 
-    )
-}
+const Dashboard = props => {
+  return (
+    <AdminLayout>
+      <div className='user_dashboard'>
+        <div>This is your dashboard</div>
+      </div>
+    </AdminLayout>
+  );
+};
 
 export default Dashboard;
