28b41a7c13286d9dadb19cf8d89c0d3e2ceebac5
maxly20 <maxly2011@gmail.com>
2021-04-28 21:19:43 +1000

    refactor promotion section

--- src/Components/Home/promotion/Enroll.js
+++ src/Components/Home/promotion/Enroll.js
@@ -1,95 +1,84 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { Fade } from 'react-awesome-reveal';
-import { CircularProgress } from '@material-ui/core'
+import { CircularProgress } from '@material-ui/core';
 
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
-import { showErrorToast, showSuccessToast } from '../../Utils/tools'
+import { showErrorToast, showSuccessToast } from '../../Utils/tools';
 import { promotionsCollection } from '../../../firebase';
 
 const Enroll = () => {
-    const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const formik = useFormik({
-        initialValues:{ email:''},
-        validationSchema: Yup.object({
-            email:Yup.string()
-            .email('Invalid email')
-            .required('The email is required')
-        }),
-        onSubmit:( values,{ resetForm} )=>{
-            setLoading(true);
-            submitForm(values)
-        }
-    });
+  const formik = useFormik({
+    initialValues: { email: '' },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email')
+        .required('The email is required'),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      setLoading(true);
+      submitForm(values);
+    },
+  });
 
+  const submitForm = async values => {
+    try {
+      const isOnTheList = await promotionsCollection
+        .where('email', '==', values.email)
+        .get();
 
-    const submitForm = async(values) => {
-        try{
-            const isOnTheList = await promotionsCollection
-            .where('email','==',values.email).get();
+      if (isOnTheList.docs.length >= 1) {
+        showErrorToast('sorry you are on the list already');
+        setLoading(false);
+        return false;
+      }
 
-            if(isOnTheList.docs.length >= 1){
-                showErrorToast('sorry you are on the list already');
-                setLoading(false);
-                return false;
-            }
-
-            ////// 
-            await promotionsCollection.add({ email: values.email });
-            formik.resetForm();
-            setLoading(false);
-            showSuccessToast('Congratulation !!!:)');
-        } catch(error){
-            showErrorToast(error)
-        }
+      //////
+      await promotionsCollection.add({ email: values.email });
+      formik.resetForm();
+      setLoading(false);
+      showSuccessToast('Congratulation !!!:)');
+    } catch (error) {
+      showErrorToast(error);
     }
+  };
 
+  return (
+    <Fade>
+      <div className='enroll_wrapper'>
+        <form onSubmit={formik.handleSubmit}>
+          <div className='enroll_title'>Enter your email</div>
+          <div className='enroll_input'>
+            <input
+              name='email'
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              placeholder='Enter your email'
+            />
 
-    return(
-        <Fade>
-           <div className="enroll_wrapper">
-                <form onSubmit={formik.handleSubmit}>
-                    <div className="enroll_title">
-                        Enter your email
-                    </div>
-                    <div className="enroll_input">
+            {formik.touched.email && formik.errors.email ? (
+              <div className='error_label'>{formik.errors.email}</div>
+            ) : null}
 
-                        <input
-                            name="email"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.email}
-                            placeholder="Enter your email"
-                        />
+            {loading ? (
+              <CircularProgress color='secondary' className='progress' />
+            ) : (
+              <button type='submit'>Enroll</button>
+            )}
 
-                        { formik.touched.email && formik.errors.email ?
-                            <div className="error_label">
-                                {formik.errors.email}
-                            </div>
-                        :null}
+            <div className='enroll_discl'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+          </div>
+        </form>
+      </div>
+    </Fade>
+  );
+};
 
-                        { loading ? 
-                            <CircularProgress color="secondary" className="progress"/>
-                            :
-                            <button
-                                type="submit"
-                            >
-                                Enroll
-                            </button>
-                        }
-
-                        <div className="enroll_discl">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-
-
-                    </div>
-                </form>
-           </div>
-        </Fade>
-    )
-}
-
-export default Enroll
+export default Enroll;
