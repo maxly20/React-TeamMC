0df3cb78a162be514e86aad4353b93dfdf42191b
maxly20 <maxly2011@gmail.com>
2021-04-28 21:19:23 +1000

    refactor promotion section

--- src/Components/Home/promotion/Animation.js
+++ src/Components/Home/promotion/Animation.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { Zoom } from 'react-awesome-reveal';
 
 const Animation = () => {
-    return(
-        <div className="promotion_animation">
-            <div className="left">
-                <Zoom>
-                    <div>
-                        <span>Win a</span>
-                        <span>Jersey</span>
-                    </div>
-                </Zoom>
-            </div>
-            <div className="right">
-                <Zoom>
-                    <div className="jersey"></div>
-                </Zoom>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className='promotion_animation'>
+      <div className='left'>
+        <Zoom>
+          <div>
+            <span>Win a</span>
+            <span>Jersey</span>
+          </div>
+        </Zoom>
+      </div>
+      <div className='right'>
+        <Zoom>
+          <div className='jersey'></div>
+        </Zoom>
+      </div>
+    </div>
+  );
+};
 
-export default Animation
+export default Animation;
