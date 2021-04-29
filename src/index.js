025e5b6184e5d3808636060dcdf5c96605cdf2b7
maxly20 <maxly2011@gmail.com>
2021-04-29 21:34:47 +1000

    building notfound page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Resources/css/app.css'
+import './Resources/css/app.css';
 import { firebase } from './firebase';
 import Routes from './routes';
 
-const App = (props) => {
-  return(
-    <Routes {...props}/>
-  )
-}
+const App = props => {
+  return <Routes {...props} />;
+};
 
-firebase.auth().onAuthStateChanged((user)=>{
-  ReactDOM.render(<App user={user}/>,document.getElementById('root'));
-})
+firebase.auth().onAuthStateChanged(user => {
+  ReactDOM.render(<App user={user} />, document.getElementById('root'));
+});
